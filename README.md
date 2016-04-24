76730ac3a51d0c47e6c30f8ea24c841e6446aca3
Mike Mathew <m2mathew@me.com>
2016-04-23 19:39:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Working through the book "Learn Python in One Day and Learn It Well"
+
+by Jamie Chan
+
+(the title needs work, right?)
---
+++ hello-world.py
@@ -0,0 +1,7 @@
+# Prints the words "Hello World"
+print ("Hello World")
+
+# This is a comment
+# This is another comment
+# This is yet another comment
+
