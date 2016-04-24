292888b49848044865b362f61a3a750dc1cd62e8
Mike Mathew <m2mathew@me.com>
2016-04-24 18:36:15 -0500

    Add variable and operator work

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,14 @@
 # Prints the words "Hello World"
 print ("Hello World")
+# newest version of python requires the parentheses in this method
+
 
 # This is a comment
 # This is another comment
 # This is yet another comment
 
+'''
+This is a comment, as well.
+It spans multiple lines
+We use three single quotes (or double quotes) for this to work
+'''
---
+++ variables.py
@@ -0,0 +1,41 @@
+# Chapter 3: The World of Variables and Operators
+
+userAge = 0
+
+userAge, userName = 30, 'Peter'
+
+'''
+Variable names can contain letters, numbers, or underscores
+But they cannot start with a number
+Can only start with a letter or underscore
+'''
+
+# Here we go
+
+x = 5
+y = 10
+# x = y
+y = x
+
+print ('x = ', x)
+print ('y = ', y)
+
+# Operator examples
+a, b = 5, 2
+
+print (a + b)
+print (a - b)
+print (a * b)
+print (a / b)
+print (a // b)
+print (a % b)
+print (a ** b)
+
+# Even more operators!
+c = 10
+# c = c + 2
+c += 2
+print (c)
+
+
+
